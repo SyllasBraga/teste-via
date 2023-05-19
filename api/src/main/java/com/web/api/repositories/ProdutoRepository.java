664ee44d152371b11c8cd550ebb82932bacd9ae5
Syllas Braga <syllasbraga2@gmail.com>
2023-05-19 08:41:37 -0300

    feat(ProdutoRepository): Inteface que herda os métodos do JpaRepository para realizar operações no banco de dados

---
+++ api/src/main/java/com/web/api/repositories/ProdutoRepository.java
@@ -0,0 +1,9 @@
+package com.web.api.repositories;
+
+import com.web.api.models.ProdutoModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProdutoRepository extends JpaRepository<ProdutoModel, Long> {
+}
